60c54fcc13ac8d181065db9f058fefeb59e5dc98
krikenah <kristenetale@gmail.com>
2023-06-22 14:16:21 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_square - Print square line dependent on the integer n.
+* @size: The number of squares/number of times
+* Return: Void.
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	}
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
