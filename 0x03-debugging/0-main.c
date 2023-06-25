d1c4dada0bf39db452b501dc5a266cc575a36687
krikenah <kristenetale@gmail.com>
2023-06-25 13:10:05 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void positive_or_negative(int num)
+{
+        if (num > 0) {
+        printf("The number is positive.\n");
+        } else if (num < 0) {
+        printf("The number is negative.\n");
+        } else {
+        printf("The number is zero.\n");
+        }
+}
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
+
