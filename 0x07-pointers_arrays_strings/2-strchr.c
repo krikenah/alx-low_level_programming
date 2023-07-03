36885f14c456692a83929cff6eff8a91d210fb63
krikenah <kristenetale@gmail.com>
2023-07-03 13:49:39 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strchr -  locates a character in a string
+ * @s: input value
+ * @c: input value
+ * Return: 0
  */
-int main(void)
+char *_strchr(char *s, char c)
 {
-	char *s = "hello";
-	char *f;
+	int i = 0;
 
-	f = _strchr(s, 'l');
-
-	if (f != NULL)
+	for (; s[i] >= '\0'; i++)
 	{
-	printf("%s\n", f);
+		if (s[i] == c)
+			return (&s[i]);
 	}
 	return (0);
 }
