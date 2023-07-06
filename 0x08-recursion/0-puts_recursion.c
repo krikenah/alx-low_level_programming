69415a032269ce3f4734129cb1ddf3eb9dbec2e5
krikenah <kristenetale@gmail.com>
2023-07-06 06:04:07 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: the string
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
